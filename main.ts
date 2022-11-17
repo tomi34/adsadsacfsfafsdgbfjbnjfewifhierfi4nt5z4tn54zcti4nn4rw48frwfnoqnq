64c9573d3fbdf10c069f9f3f44ef201876b719dd
tomi34 <66636695+tomi34@users.noreply.github.com>
2022-11-17 12:30:36 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,31 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    player.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.move(1)
+})
+let player: game.LedSprite = null
 let meteor: game.LedSprite = null
 let speed = 500
 meteor = game.createSprite(randint(0, 4), 0)
 meteor.set(LedSpriteProperty.Direction, 0)
 meteor.set(LedSpriteProperty.Brightness, 255)
-let player = game.createSprite(randint(0, 4), 4)
+player = game.createSprite(randint(0, 4), 4)
 player.set(LedSpriteProperty.Brightness, 150)
 game.startCountdown(60000)
 game.setScore(0)
 basic.forever(function () {
     pause(speed)
 meteor.move(-1)
-if (meteor.isTouching(player)) {
-    game.gameOver()
-}
+    if (meteor.isTouching(player)) {
+        game.gameOver()
+    }
     if (meteor.get(LedSpriteProperty.Y) == 4) {
         pause(speed)
-        game.addScore(1)
-
-meteor.setY(0)
+game.addScore(1)
+        meteor.setY(0)
 meteor.setX(randint(0, 4))
 speed = speed - 15
     }
-})
-
-input.onButtonPressed(Button.A, function() {
-    player.move(-1)
-})
-input.onButtonPressed(Button.B, function () {
-    player.move(1)
 })
